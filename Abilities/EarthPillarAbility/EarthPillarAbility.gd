4abf039ff5b1808395c047ed15639dc43ba5b52a
Yuyin Li <justrodneylee@gmail.com>
2024-05-05 15:05:20 -0700

    Rock Pillar Model
    Added Rock Pillar Model
    Added Rock Pillar Cast Animation + Cast Delay
    Minor tweaks to animations

--- Abilities/EarthPillarAbility/EarthPillarAbility.gd
+++ Abilities/EarthPillarAbility/EarthPillarAbility.gd
@@ -2,8 +2,13 @@ extends Node
 
 const earthPillarTscn: PackedScene = preload("./EarthPillar/EarthPillar.tscn")
 @onready var playerNode: Player = $"../../"
+# TODO add gem color change
+#@onready var gemNode: GDScript = playerNode
+@export var animDelay := 0.75
 
 func spawn_pillar():
+	# No animation events means we have to time the delay ourselves
+	await get_tree().create_timer(animDelay).timeout
 	var pillar = earthPillarTscn.instantiate()
 	pillar.position = playerNode.position
 	get_tree().current_scene.add_child(pillar)
